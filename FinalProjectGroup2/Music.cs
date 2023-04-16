fe0f8d9a9f4174c7b585f52ab6b4677e7abaa53c
bocockaa <130606842+bocockaa@users.noreply.github.com>
2023-04-16 10:46:25 -0400

    Add files via upload

---
+++ FinalProjectGroup2/Music.cs
@@ -0,0 +1,15 @@
+﻿
+using System;
+
+namespace FinalProjectGroup2
+{
+    public class Music
+    {
+        public int Id { get; set; }
+        public string title { get; set; }
+        public string artist { get; set; }
+        public string musicGenre { get; set; }
+        public DateTime releaseDate { get; set; }
+        public string musicAlbum { get; set; }
+    }
+}
---
+++ FinalProjectGroup2/MusicContext.cs
@@ -0,0 +1,19 @@
+﻿namespace FinalProjectGroup2.Data
+{
+    public class FoodContext : DbContext
+    {
+
+        public FoodContext(DbContextOptions<FMusicContext> options) : base(options) { }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            builder.Entity<Music>().HasData(
+                new Music { Id = 1234, title = "Example",  artist = "Example", musicGenre = "Example", releaseDate = "01/22/2019", musicAlbum = "Example" }
+
+            );
+        }
+
+        public DbSet<Music> Musicc { get; set; }
+
+    }
+}
