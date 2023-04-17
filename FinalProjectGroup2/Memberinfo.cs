351bc89d8f711ea122888ad5771ab83706e292a6
cowdrejs <cowdrejs@mail.uc.edu>
2023-04-17 19:15:36 -0400

    Justin first push

---
+++ FinalProjectGroup2/Memberinfo.cs
@@ -0,0 +1,24 @@
+using System
+
+namespace FinalProjectGroup2
+{
+
+
+    public class Memberinfo
+    {
+        //names
+        public string FullName {get; set;}
+
+        //birthdays
+        public DateTime Birthday {get; set;}
+
+        //college program
+        public string Program {get; set;}
+
+        //Year in program
+        public int Year {get; set;}
+
+        
+    }
+
+}
