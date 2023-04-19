ae6fec37bf16a6497f4f7f7654119f9bd1798fc5
cowdrejs <cowdrejs@mail.uc.edu>
2023-04-19 10:58:55 -0400

    Update MemberinfoController.cs

--- FinalProjectGroup2/MemberinfoController.cs
+++ FinalProjectGroup2/MemberinfoController.cs
@@ -20,6 +20,68 @@ namespace FinalProjectGroup2.Controllers
         }
 
         [HttpGet]
-        public async Task<ActionResult<IEnumerable<Memberinfo>>> GetMemberinfo(int? id)
+        public async Task<ActionResult<IEnumerable<Member>>> GetMemberinfo(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return await _context.Memberinfos.Take(5).ToListAsync();
+            }
+            else
+            {
+                return await _context.Memberinfos.Where(TaskCompletionSource => TaskCompletionSource.Id == id).ToListAsync();
+            }
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Member>> GetMemberinfo(int id)
+        {
+            var memberInfos = await _context.Memberinfos.FindAsync(id);
+            
+            if (memberInfos == null)
+            {
+                return NotFound();
+            }
+            
+            return memberInfo;
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<Member>> PostMemberinfo(Member memberInfos)
+        {
+            _context.MemberInfos.Add(memberInfo);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction(nameof(GetMemberInfos), new {id = memberInfos.Id}, memberInfo);
+        }
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutMemberinfo(int id, Member memberInfo)
+        {
+            if (id != memberInfo.Id)
+            {
+                return Badrequest();
+            }
+
+            _context.Entry(memberInfo).State = EntityState.Modified;
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteMemberinfo(int id)
+        {
+            var memberInfo = await _context.MemberInfos.FindAsync(id);
+
+            if (memberInfo == null)
+            {
+                return NotFound();
+            }
+
+            _context.MemberInfos.Remove(memberInfo);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
     }
 }
