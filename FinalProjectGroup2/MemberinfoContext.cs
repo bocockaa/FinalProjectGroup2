8eef77e9ff05eb895a4f90dd1bf04133ea8ea4d0
cowdrejs <130619749+cowdrejs@users.noreply.github.com>
2023-04-25 14:52:34 -0400

    justin final

--- FinalProjectGroup2/MemberinfoContext.cs
+++ FinalProjectGroup2/MemberinfoContext.cs
@@ -10,9 +10,9 @@ namespace FinalProjectGroup2.Data
         protected override void OnModelCreating(ModelBuilder builder)
         {
             builder.Entity<Member>().HasData(
-                new Member {Id = 1, FullName = "Justin Cowdrey", Birthday = "1999, 5, 30", Program = "IT", Year = "senior"}
-                new Member {Id = 2, FullName = "Rachel Lindquist", Birthday = "2000, 5, 26", Program = "Computer Science", Year = "senior"}
-                new Member {Id = 3, FullName = "Athena Bocock", Birthday = "1999, 2, 15", Program = "IT", Year = "senior"}
+                new Member {Id = 1, FullName = "Justin Cowdrey", Birthday = "1999, 5, 30", Program = "IT", Year = "senior"},
+                new Member {Id = 2, FullName = "Rachel Lindquist", Birthday = "2000, 5, 26", Program = "Computer Science", Year = "senior"},
+                new Member {Id = 3, FullName = "Athena Bocock", Birthday = "1999, 2, 15", Program = "IT", Year = "senior"},
                 new Member {Id = 4, FullName = "Osama Bondogji", Birthday = "2001, 7, 7", Program = "IT", Year = "junior"}
             );
         }
